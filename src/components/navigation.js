7e31c42e65471c92265687cb2b75e4380957e281
Bruce Franz <bfranz@ithaca.edu>
2021-02-06 12:12:00 -0500

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -10,18 +10,6 @@ const MenuItems = [
     path: "/",
     title: "Home"
   },
-  {
-    path: "/about",
-    title: "About"
-  },
-  {
-    path: "/blog",
-    title: "Blog"
-  },
-  {
-    path: "/contact",
-    title: "Contact"
-  },
 ]
 
 const ListLink = (props) => (<li><Link to={props.to}>{props.children}</Link></li>)
